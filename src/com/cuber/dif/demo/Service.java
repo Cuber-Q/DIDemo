9df9077bf5faaf441eb5a110087678cfe8414de3
Cuber-Q <qu.peng@foxmail.com>
2016-06-01 03:14:04 +0800

    add .gitignore file

--- src/com/cuber/dif/demo/Service.java
+++ src/com/cuber/dif/demo/Service.java
@@ -5,6 +5,6 @@ import com.cuber.dif.framework.basis.annotation.Inject;
 @Inject
 public class Service {
 	public void doSthElse(){
-		System.out.println("[Service] I will do something else");
+		System.out.println("[Service] I will do something else.");
 	}
 }
